87e092796af9dadc06e13ecde43e77b2ba0431b0
Kaue Caponero Figueiredo <kauecaponero@hotmail.com>
2023-05-17 18:17:59 -0300

    Corringindo entry

--- Java/ChapterOne/src/services/AutorService.java
+++ Java/ChapterOne/src/services/AutorService.java
@@ -432,8 +432,8 @@ public class AutorService {
 			            Livro livro_lista = livro_na_lista.getValue();
 			            if (livro_autor.equals(livro_lista)) {
 			                boolean autorAtualizado = false;
-			                for (Map.Entry<Integer, Autor> entry : livro_lista.getAutores_livro().entrySet()) {
-			                    Autor autor = entry.getValue();
+			                for (Map.Entry<Integer, Autor> autor_livro : livro_lista.getAutores_livro().entrySet()) {
+			                    Autor autor = autor_livro.getValue();
 			                    if (autor.getId_autor() == autor_editar.getId_autor()) {
 			                        autorAtualizado = true;
 			                        break;
