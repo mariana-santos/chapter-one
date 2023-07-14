86b51644d67f015296d5134a10fda7500f9784d5
mariana-santos <marianasfernandessousa@gmail.com>
2023-07-14 17:50:03 -0300

    ajuste no front

--- React/src/Pages/Home/index.js
+++ React/src/Pages/Home/index.js
@@ -41,14 +41,9 @@ export default function Home(){
     return(
         <>
             <Header />
-            <div className="container">
+            <div className="container" id="home">
 
                 <Sobre />
-
-                <a id="saiba_mais" href="#livros">
-                    Deslize para ver mais
-                    <BsChevronCompactDown />
-                </a>
 
                 <h2 id="livros"> Mais populares </h2>
                 <ListaLivros livros={livros} />
@@ -72,7 +67,12 @@ function Sobre(){
                 <p>Chapter One é uma livraria encantadora fundada por <strong>Kaue Caponero Figueiredo</strong> e <strong>Mariana Santos Fernandes de Sousa</strong> em 2023. Desde então, nossa livraria vem crescendo e se destacando cada vez mais no mercado. </p>
 
                 <p>Conhecida por seu acervo diversificado de livros de todos os gêneros imagináveis, desde clássicos literários até as obras mais recentes de autores renomados, temos preços acessíveis e <strong>descontos de até 40%!</strong> </p>
-            </div>
+            </div> 
+
+            <a id="saiba_mais" href="#livros">
+                    Deslize para ver mais
+                    <BsChevronCompactDown />
+                </a>
         </section>
     )
 }
